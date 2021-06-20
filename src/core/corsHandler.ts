a34b595786ad309a80b73f679a5b59e89ed00f7c
luca <lucaperullo@outlook.it>
2021-06-20 12:33:50 +0200

    up

--- src/core/corsHandler.ts
+++ src/core/corsHandler.ts
@@ -1,18 +1,26 @@
-import cors from "cors"
-import { Request, Response, NextFunction } from "express"
+import cors from "cors";
+import { Request, Response, NextFunction } from "express";
 
-export const corsHandler = (req?: Request, res?: Response, next?: NextFunction) => {
-  const whiteListedDomains = ["http://localhost:3000", "https://whatzapp.vercel.app"]
+export const corsHandler = (
+  req?: Request,
+  res?: Response,
+  next?: NextFunction
+) => {
+  const whiteListedDomains = [
+    "http://localhost:3000",
+    "https://whatzapp.vercel.app",
+    "http://localhost:8100/",
+  ];
 
   const corsOptionsDelegate = function (req, callback) {
-    let corsOptions
+    let corsOptions;
     if (whiteListedDomains.indexOf(req.header("Origin")) !== -1) {
-      corsOptions = { origin: true, optionsSuccessStatus: 200 } // reflect (enable) the requested origin in the CORS response
+      corsOptions = { origin: true, optionsSuccessStatus: 200 }; // reflect (enable) the requested origin in the CORS response
     } else {
-      corsOptions = { origin: false } // disable CORS for this request
+      corsOptions = { origin: false }; // disable CORS for this request
     }
-    callback(null, corsOptions) // callback expects two parameters: error and options
-  }
+    callback(null, corsOptions); // callback expects two parameters: error and options
+  };
 
-  return cors(corsOptionsDelegate)
-}
+  return cors(corsOptionsDelegate);
+};
